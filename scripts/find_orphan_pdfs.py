3240a503e47764c13fdc3f51c1e47f475d967a5a
dth@taiga.ai <DannyDannyDanny>
2022-08-23 15:44:38 +0200

    :boom: work on replacing PDFs with SVGs

--- scripts/find_orphan_pdfs.py
+++ scripts/find_orphan_pdfs.py
@@ -1,6 +1,35 @@
 from pathlib import Path
 
-print('PDFs in `./figures/` that should be converted to SVG or PNG:')
+# iterate all pdfs in `figures/**/*pdf`
+pdfpaths = Path('figures').rglob('*.pdf')
 
-for p in Path('figures').rglob('*.pdf'):
-    print('\t\t * [ ]', p)
+for pdfpath in pdfpaths:
+    pdfpathstr = str(pdfpath)
+    svgpathstr = pdfpathstr.replace('.pdf', '.svg')
+    print(f'replacing `{pdfpathstr}` with `{svgpathstr}`')
+    
+    # iterate all srccode in `figures/**/*.py`
+    srcpaths = Path('code').rglob('*.py')
+    for srcpath in srcpaths:
+        srccode = srcpath.read_text()
+
+        if pdfpathstr in srccode:
+            print(f'\treplaced in {srcpath}')
+        # srccode_svg = srccode.replace(pdfpathstr, svgpathstr)
+        # srccodepath.write_text(srccode_svg)
+
+    # iterate all srccode in `rst/**/*.rst`
+    rstpaths = Path('rst').rglob('*.rst')
+    for rstpath in rstpaths:
+        rstcontent = rstpath.read_text()
+        
+        if pdfpathstr in rstcontent:
+            print(f'\treplaced in {rstpath}')
+        # rstcontent_svg = rstcontent.replace(pdfpathstr, svgpathstr)
+        # rstpath.write_text(srccode_svg)
+    break
+            
+
+# print(pdfpaths)
+# print(srccodepaths)
+# find all occurences of pdfpath in `code/**/*.py`
