75bfe319ddfc85afb032e9eeddf94439999a7dc4
dth@taiga.ai <DannyDannyDanny>
2022-08-28 14:01:44 +0200

    :memo: :zap: :bug: :boom: run subset, fix fonts

--- scripts/remake_all_figures.py
+++ scripts/remake_all_figures.py
@@ -6,8 +6,19 @@ that way the relative paths in each script doesn't have to be changed
 
 import os
 from pathlib import Path
+from matplotlib import font_manager
+
+print('adding font files')
+font_files = font_manager.findSystemFonts(fontpaths='fonts/')
+for font_file in font_files:
+    print(font_file)
+    font_manager.fontManager.addfont(font_file)
+
 srcpaths = Path('code').rglob('*.py')
+# only first 5 files for now
+srcpaths = list(srcpaths)[:5]
+sep = 80*'-'
 
 for srcpath in srcpaths:
-    print(f'exec: `cd {srcpath.parent} && python3 {srcpath.name}`')
+    print(f'{sep}\nexec: `cd {srcpath.parent} && python3 {srcpath.name}`')
     os.system(f'cd {srcpath.parent} && python3 {srcpath.name}')
